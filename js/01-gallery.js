4ce87070a75bb3991d4a8362940f66d6ef2fafd8
Nataliia-Katrenko <notika1406@ukr.net>
2023-09-30 08:18:30 +0300

    Сompleted tasks added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+//? making murkup fot ul.gallery
+const photoEl = galleryItems
+  .map((e) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${e.original}">
+    <img
+      class="gallery__image"
+      src="${e.preview}"
+      data-source="${e.original}"
+      alt="${e.description}"
+    />
+  </a>
+</li>`;
+  })
+  .join(" ");
+ulEl.innerHTML = photoEl;
+
+ulEl.addEventListener("click", onImgClick);
+
+//? removing default changes on link
+const allLinkBigImg = document.querySelectorAll(".gallery__link");
+allLinkBigImg.forEach((a) =>
+  a.addEventListener("click", (e) => e.preventDefault())
+);
+
+//? getting big url after img click
+function onImgClick(event) {
+  if (event.target.tagName === "IMG") {
+    const modal = basicLightbox.create(
+      `
+    <img src="${event.target.dataset.source}"></img>
+    `,
+      {
+        onShow: () => {
+          document.addEventListener("keyup", evBinded);
+        },
+        onClose: () => {
+          document.removeEventListener("keyup", evBinded);
+        },
+      }
+    );
+    const evBinded = ev.bind(modal);
+    modal.show();
+  }
+}
+function ev(e) {
+  if (e.code === "Escape") {
+    this.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const ulEl = document.querySelector(".gallery");
+
+//? making markup in ul
+const ulMarkup = galleryItems
+  .map((e) => {
+    return `<li class="gallery__item">
+             <a class="gallery__link" href="${e.original}">
+                <img class="gallery__image" src="${e.preview}" alt="${e.description}" />
+            </a>
+          </li>`;
+  })
+  .join(" ");
+ulEl.insertAdjacentHTML("beforeend", ulMarkup);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250
+});
